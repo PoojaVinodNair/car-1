8976789981b2598103f822a6f3727b519cedd88a
PoojaVinodNair <71579268+PoojaVinodNair@users.noreply.github.com>
2020-11-18 15:56:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var canvas,backgroundImage;
+var playerCount;
+var player,game,form;
+
+function setup(){
+  database = firebase.database();
+  canvas=createCanvas(500,500);
+game=new Game();
+game.getState();
+game .start();
+  
+}
+
+function draw(){
+  background("white");
+  
+   
+    
+  
+}
+
